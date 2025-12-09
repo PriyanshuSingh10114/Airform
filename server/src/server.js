2f137fd09228ec2f80fc231e3dc75a723ee595d0
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-12-09 19:00:22 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 // CORS needs credentials enabled for sessions to work
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://airform-tau.vercel.app/",
   credentials: true
 }));
 
